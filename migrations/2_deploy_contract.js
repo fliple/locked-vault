9446d543a379657bc0d28de1cec43a3243162f10
Yada Tashy <88944220+yadatashy@users.noreply.github.com>
2021-09-12 15:06:51 +0300

    Update migration token address

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,8 +2,8 @@ const TokenLocker = artifacts.require('TokenLocker');
 
 module.exports = function(deployer) {
   deployer.deploy(
-    TokenLocker, 
-    '0xb6505dEfE58759C09e0dF0739f8F5A6f32bffd44',
+    TokenLocker,
+    '0xC59615DA2DA226613B1C78F0c6676CAC497910bC',
     '1800',
     '900000000'
   );
